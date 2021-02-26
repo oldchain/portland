c8e2d224b4bda4bef418eabef325dfeeb3ca4c78
oldchain <hussein.adel29@yahoo.com>
2021-02-26 15:05:40 +0200

    adding the best seller section and product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
+IG.trigger({   triggerNames: ['SqlTrigger1'],   urlParams: {} }) .done(  function (result) {   alert('تم الحفظ ') })
